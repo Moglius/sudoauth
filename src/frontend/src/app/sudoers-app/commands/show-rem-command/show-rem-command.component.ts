0e6fec73484d10ea73bb20a662966671280e661e
Matias Moglia <matiasmoglia@gmail.com>
2023-04-16 16:54:36 -0300

    adding commands

--- src/frontend/src/app/sudoers-app/commands/show-rem-command/show-rem-command.component.ts
+++ src/frontend/src/app/sudoers-app/commands/show-rem-command/show-rem-command.component.ts
@@ -12,6 +12,13 @@ export class ShowRemCommandComponent implements OnInit{
   commandsList: any = [];
   next: string = '';
   previous: string = '';
+  modalTitle: string = '';
+  activateAddEditComponent: boolean = false;
+  commands_dep = {};
+
+  commandsFilter: string = "";
+  commandsListWithoutFilter: any = [];
+  addButtonText = 'Add Command';
 
   constructor(private service: LnxuserService) {}
 
@@ -22,6 +29,7 @@ export class ShowRemCommandComponent implements OnInit{
   refreshCommandsList(url: string) {
     this.service.getLnxUsersList(url).subscribe(data=>{
       this.commandsList = data.results;
+      this.commandsListWithoutFilter = data.results;
 
       if (data.next) {
         this.next = data.next;
@@ -45,6 +53,53 @@ export class ShowRemCommandComponent implements OnInit{
 
   fetchPrevious() {
     this.refreshCommandsList(this.previous);
+  }
+
+  addClick(){
+    this.commands_dep = {
+      'command': {
+        'diggest': '',
+        'command': '',
+        'args': ''
+      },
+      'edit': false
+    };
+    this.modalTitle = 'Add Command';
+    this.activateAddEditComponent = true;
+  }
+
+  editClick(command: any){
+    this.commands_dep = {
+      'command': command,
+      'edit': true
+    };
+    this.modalTitle = 'Edit Command';
+    this.activateAddEditComponent = true;
+  }
+
+  deleteClick(command: any) {
+    if (confirm('are you sure?')) {
+      this.service.deleteEntry(this.apiurl + command.pk + '/').subscribe(data => {
+        this.refreshCommandsList(this.apiurl);
+      });
+    }
+  }
+
+  closeClick(){
+    this.activateAddEditComponent = false;
+    this.refreshCommandsList(this.apiurl);
+    this.commands_dep = {};
+  }
+
+  FilterFn(){
+    var commandsFilter = this.commandsFilter;
+
+    this.commandsList = this.commandsListWithoutFilter.filter(function (command: any){
+        return command.full_command.toString().toLowerCase().includes(
+          commandsFilter.toString().trim().toLowerCase()
+        )
+    });
+
   }
 
 }
