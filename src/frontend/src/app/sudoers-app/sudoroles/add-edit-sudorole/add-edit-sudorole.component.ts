2dc2edbf22b18b07ff1bbbaef2b0cc9a66cab12b
Matias Moglia <matiasmoglia@gmail.com>
2024-04-21 17:43:27 -0300

    sudorule app working

--- src/frontend/src/app/sudoers-app/sudoroles/add-edit-sudorole/add-edit-sudorole.component.ts
+++ src/frontend/src/app/sudoers-app/sudoroles/add-edit-sudorole/add-edit-sudorole.component.ts
@@ -1,6 +1,7 @@
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LnxuserService } from 'src/app/lnxuser.service';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-add-edit-sudorole',
@@ -9,7 +10,11 @@ import { LnxuserService } from 'src/app/lnxuser.service';
 })
 export class AddEditSudoroleComponent implements OnInit{
 
-  constructor(private service: LnxuserService, private router: Router) {  }
+  constructor(
+    private service: LnxuserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {  }
 
   @Output() closeChild: EventEmitter<any> = new EventEmitter();
 
@@ -18,11 +23,45 @@ export class AddEditSudoroleComponent implements OnInit{
   role_name: any;
   commands: any[] = [];
   targetCommands: any[] = [];
+  id: string = "0";
 
   selectedCommands: any[] = [];
   selectedSelectedCommands: any[] = [];
 
-  updateRole(role: any){
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.refreshCommandList(this.commandsUrl, this.id);
+  }
+
+  refreshCommandList(url: string, id: string = '0') {
+    this.service.getLnxUsersList(url).subscribe(data=>{
+      this.commands = data.results;
+      if (id !== "0") {
+        this.service.getLnxUsersList(this.rolesUrl + this.id + '/').pipe(
+          catchError(() => [this.router.navigate(["/sudoroles"])]),
+        ).subscribe(data => {
+          this.role_name = data.name;
+          this.selectedCommands = data.commands;
+          this.moveSelectedCommands(this.selectedCommands);
+        });
+      }
+    });
+  }
+
+  updateRole(id: string, targetCommands: any[]){
+    let commands: Number[] = [];
+
+    targetCommands.forEach(element => {
+      commands.push(element["pk"]);
+    });
+
+    let role = {
+      "commands": commands
+    };
+
+    this.service.updateEntry(this.rolesUrl + id + "/", role).subscribe(() => {
+      this.router.navigate(["/sudoroles"]);
+    });
   }
 
   addRole(role_name: string, selectedCommands: any[]){
@@ -44,22 +83,11 @@ export class AddEditSudoroleComponent implements OnInit{
     });
   }
 
-  ngOnInit(): void {
-    this.refreshCommandList(this.commandsUrl);
-  }
-
-  refreshCommandList(url: string) {
-    this.service.getLnxUsersList(url).subscribe(data=>{
-      this.commands = data.results;
-    });
-  }
-
   moveSelectedCommands(sourceCommands: any[]) {
-
     const selectedCommands = [...sourceCommands.filter(option => this.selectedCommands.includes(option))];
-    this.commands = this.commands.filter( function( el ) {
-      return !selectedCommands.includes( el );
-    });
+    this.commands = this.commands.filter( host_elem => !selectedCommands.find(
+      sel_elem => (host_elem.pk === sel_elem.pk)
+    ));
     this.targetCommands = [...new Set([...this.targetCommands, ...selectedCommands])];
   }
 
